e1534249e622ccf7291857094915d861ed022a66
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-05-19 14:06:22 -0300

    primeiro commit

---
+++ bloco-4/dia1/scripts.js
@@ -0,0 +1,8 @@
+let name = "Gustavo Jezini"
+const birthCity = "Sao Jose dos Campos"
+let birthYear = 2001
+console.log(name)
+birthCity = "oitra"
+console.log(birthCity)
+birthYear = 2030
+console.log(birthYear)
