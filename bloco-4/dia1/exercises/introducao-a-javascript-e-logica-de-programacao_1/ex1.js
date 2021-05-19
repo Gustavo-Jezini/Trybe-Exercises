87ef5f6c7c1094150665631ade7687c55b4b0778
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-05-19 16:16:02 -0300

    exercicio 2, comparar numeros

--- bloco-4/dia1/exercises/introducao-a-javascript-e-logica-de-programacao_1/ex1.js
+++ bloco-4/dia1/exercises/introducao-a-javascript-e-logica-de-programacao_1/ex1.js
@@ -1,27 +1,40 @@
-let calculo ="eeer"
-let a = 8 
-let b = 4
+// let calculo ="eeer"
+// let a = 8 
+// let b = 4
 
-if(calculo=="adicao") {
-  console.log(a + b)
+// if(calculo=="adicao") {
+//   console.log(a + b)
+// }
+
+// else if(calculo=="subtracao") {
+//   console.log(a - b)
+// }
+
+// else if(calculo=="multiplicacao") {
+//   console.log(a*b)
+// }
+
+// else if(calculo=="divisao") {
+//   console.log(a/b)
+// }
+
+// else if(calculo=="modulo") {
+//   console.log(a%b)
+// }
+
+// else {
+//   console.log("operacao invalida")
+// }
+
+let x = 10
+let y = 11
+
+if (x > y) {
+  console.log(x)
 }
-
-else if(calculo=="subtracao") {
-  console.log(a - b)
+else if (y > x) {
+  console.log(y)
 }
-
-else if(calculo=="multiplicacao") {
-  console.log(a*b)
-}
-
-else if(calculo=="divisao") {
-  console.log(a/b)
-}
-
-else if(calculo=="modulo") {
-  console.log(a%b)
-}
-
 else {
-  console.log("operacao invalida")
+  console.log("sao iguais")
 }
