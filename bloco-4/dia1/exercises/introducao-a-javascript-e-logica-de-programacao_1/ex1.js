33aa3d8c44d78225edd51c7a3a3af123655707c7
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-05-19 18:07:36 -0300

    exercicio 9

--- bloco-4/dia1/exercises/introducao-a-javascript-e-logica-de-programacao_1/ex1.js
+++ bloco-4/dia1/exercises/introducao-a-javascript-e-logica-de-programacao_1/ex1.js
@@ -79,11 +79,22 @@
 //   console.log("wtf")
 // }
 
+// let a = 3
+// let b = 4
+// let c = 5
+
+// if (a%2==0 || b%2==0 || c%2==0) {
+//   console.log(true)
+// }
+// else {
+//   console.log(false)
+// }
+
 let a = 3
-let b = 7
+let b = 4
 let c = 5
 
-if (a%2==0 || b%2==0 || c%2==0) {
+if (a%2==1 || b%2==1 || c%2==1) {
   console.log(true)
 }
 else {
