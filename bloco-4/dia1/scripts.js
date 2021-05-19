ae744fcfefa0891e69a9218b635fb9f23756a47c
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-05-19 15:18:39 -0300

    finalizado parte 3

--- bloco-4/dia1/scripts.js
+++ bloco-4/dia1/scripts.js
@@ -7,15 +7,22 @@
 //   birthYear = 2030
 //   console.log(birthYear)
 
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
-console.log(typeof patientId)
-console.log(typeof isEnrolled)
-console.log(typeof patientInfo)
-console.log(typeof patientEmail)
-console.log(typeof patientAge)
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+// console.log(typeof patientId)
+// console.log(typeof isEnrolled)
+// console.log(typeof patientInfo)
+// console.log(typeof patientEmail)
+// console.log(typeof patientAge)
+
+let base = 5;
+let altura = 8;
+let area = base*altura;
+console.log(area)
+let perimetro = base*2 + altura*2;
+console.log(perimetro)
