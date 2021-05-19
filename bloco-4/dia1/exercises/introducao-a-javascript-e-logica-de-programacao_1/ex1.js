b2cf96944d1c8dcc5f6445da9593949004f2ba87
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-05-19 16:11:48 -0300

    primeiro Exercicio

---
+++ bloco-4/dia1/exercises/introducao-a-javascript-e-logica-de-programacao_1/ex1.js
@@ -0,0 +1,27 @@
+let calculo ="eeer"
+let a = 8 
+let b = 4
+
+if(calculo=="adicao") {
+  console.log(a + b)
+}
+
+else if(calculo=="subtracao") {
+  console.log(a - b)
+}
+
+else if(calculo=="multiplicacao") {
+  console.log(a*b)
+}
+
+else if(calculo=="divisao") {
+  console.log(a/b)
+}
+
+else if(calculo=="modulo") {
+  console.log(a%b)
+}
+
+else {
+  console.log("operacao invalida")
+}
