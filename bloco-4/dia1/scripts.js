787cfebf3e8fd4a0a8dcffb244d812c23ce461db
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-05-19 15:14:54 -0300

    primeiros ex , da parte 3

--- bloco-4/dia1/scripts.js
+++ bloco-4/dia1/scripts.js
@@ -1,8 +1,21 @@
-let name = "Gustavo Jezini"
-const birthCity = "Sao Jose dos Campos"
-let birthYear = 2001
-console.log(name)
-birthCity = "oitra"
-console.log(birthCity)
-birthYear = 2030
-console.log(birthYear)
+//   let name = "Gustavo Jezini"
+//   const birthCity = "Sao Jose dos Campos"
+//   let birthYear = 2001
+//   console.log(name)
+//   birthCity = "oitra"
+//   console.log(birthCity)
+//   birthYear = 2030
+//   console.log(birthYear)
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+console.log(typeof patientId)
+console.log(typeof isEnrolled)
+console.log(typeof patientInfo)
+console.log(typeof patientEmail)
+console.log(typeof patientAge)
