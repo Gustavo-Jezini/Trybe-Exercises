453d733cd6db1308650d9d25d015ed54cff7ab83
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-05-20 14:40:38 -0300

    ultimo exercicio das aulas assincronas

--- bloco-4/dia2/scripts.js
+++ bloco-4/dia2/scripts.js
@@ -8,6 +8,19 @@
 
 // console.log(indexOfPortfolio);
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push = 'Contato'
-console.log(menu);
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push = 'Contato'
+// console.log(menu);
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for  (let index = 0 ; index < groceryList.length ; index += 1 )
+// {
+//   console.log(groceryList[index])
+// }
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for ( letter of names) {
+  console.log(letter)
+}
