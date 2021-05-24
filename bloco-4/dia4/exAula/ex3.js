49a396a07e96266df5100141fc29a24d028725c0
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-05-24 13:04:01 -0300

    exercicio funcoes refatoracao

---
+++ bloco-4/dia4/exAula/ex3.js
@@ -0,0 +1,25 @@
+let a = 10
+let b = 5
+
+function soma (a ,b) {
+  return (a + b)
+};
+
+function subtracao (a , b) {
+  return (a - b)
+};
+
+function multiplication ( a, b ){
+  return (a*b)
+};
+
+function divisao ( a, b ) {
+  return (a/b)
+};
+
+function modulo (a,b) {
+  return (a%b)
+}; 
+
+
+console.log(soma(10, 5))
