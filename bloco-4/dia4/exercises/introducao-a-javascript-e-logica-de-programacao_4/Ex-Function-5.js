d444e69be463a30a90cb0ed40fe161a7bbd23e59
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-05-25 10:15:22 -0300

    eu fiz sozin

--- bloco-4/dia4/exercises/introducao-a-javascript-e-logica-de-programacao_4/Ex-Function-5.js
+++ bloco-4/dia4/exercises/introducao-a-javascript-e-logica-de-programacao_4/Ex-Function-5.js
@@ -4,41 +4,36 @@ let contRepetido = 0;
 let contNumero = 0;
 let indexNumeroRepetido = 0;
 
-function contagem (numeros) {
+// function contagem (numeros) {
+//   for (let index in numeros) {
+//     let verificaNumero = numeros[index]
+//     for (let index2 in numeros) {
+//       if (numeros[index2] === verificaNumero) {
+//         contNumero += 1;
+//       }
+//       if (contRepetido < contNumero) {
+//         contNumero = contRepetido;
+//         indexNumeroRepetido = index ; 
+//       }
+//     }contNumero = 0;
+//   } return numbers[indexNumeroRepetido] ; 
+// }
+
+// console.log(contagem(numbers));
+
+
+function contador(numeros) {
   for (let index in numeros) {
-    let verificaNumero = numeros[index]
-    for (let index2 in numeros) {
-      if (numeros[index2] === verificaNumero) {
-        contNumero += 1;
+    let verifica = numeros[index] 
+    for (let index2 in numeros[index]) {
+      if (verifica == numeros[index2]) {
+        contNumero += 1
       }
-      if (contRepetido < contNumero) {
+      if(contRepeticao < contNumero) {
         contNumero = contRepetido;
-        indexNumeroRepetido = index ; 
+        indexNumeroRepetido = index; 
       }
-    }contNumero = 0;
-  } return numbers[indexNumeroRepetido] ; 
+    } contNumero = 0
+  } return numeros[indexNumeroRepetido]
 }
-
-console.log(contagem(numbers));
-
-//Duas formas de resolver o exercício
-function maisRepetido(numeros) {
-  let contRepetido = 0;
-  let contNumero = 0;
-  let indexNumeroRepetido = 0;
-  for (let index in numeros) {
-    let verificaNumero = numeros[index];
-    for (let index2 in numeros) {
-      if (verificaNumero === numeros[index2]) {
-        contNumero += 1;
-      }
-    }
-    if (contNumero > contRepetido) {
-      contRepetido = contNumero;
-      indexNumeroRepetido = index;
-    }
-    contNumero = 0;
-  }
-  return numeros[indexNumeroRepetido];
-}
-
+console.log(contador(numbers))
