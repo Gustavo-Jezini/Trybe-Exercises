91fbeedeead16e04b3a1962046ee530793ccce23
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-05-27 15:54:32 -0300

    fiz tudo, quase tudo

---
+++ bloco-5/exercises/javascript-dom-eventos-e-web-storage_2/script.js
@@ -0,0 +1,7 @@
+document.getElementsByTagName("body")[0].id = "corpo"
+
+let titulo = document.createElement('h1')
+
+document.getElementsByTagName("h1")[0].innerHTML =  "Exercício 5.2 - JavaScript DOM"
+
+corpo.appendChild(document.getElementsByTagName("h1")[0])
