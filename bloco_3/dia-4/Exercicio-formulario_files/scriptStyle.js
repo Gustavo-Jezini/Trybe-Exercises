07b06bf87ddbb12502f9d30069438862883f31c3
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-06-11 11:16:47 -0300

    enviando arquivos antigos

---
+++ bloco_3/dia-4/Exercicio-formulario_files/scriptStyle.js
@@ -0,0 +1,35 @@
+let listOfStates = ["Acre",
+  "Alagoas",
+  "Amapá",
+  "Amazonas",
+  "Bahia",
+  "Ceará",
+  "Distrito Federal",
+  "Espirito Santo",
+  "Goiás",
+  "Maranhão",
+  "Mato Grosso do Sul",
+  "Mato Grosso",
+  "Minas Gerais",
+  "Pará",
+  "Paraíba",
+  "Paraná",
+  "Pernambuco",
+  "Piauí",
+  "Rio de Janeiro",
+  "Rio Grande do Norte",
+  "Rio Grande do Sul",
+  "Rondônia",
+  "Roraima",
+  "Santa Catarina",
+  "São Paulo",
+  "Sergipe",
+  "Tocantins"];
+  let statesOptions = document.getElementById('state')
+
+  for(let i = 0; i < listOfStates.length; i += 1) {
+    let option = document.createElement('option');
+    option.innerText = listOfStates[i];
+    statesOptions.appendChild(option);
+  }
+  
