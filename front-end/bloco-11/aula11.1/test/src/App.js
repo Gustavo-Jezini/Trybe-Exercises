3dd7f3963bf5ec73ce0c85c04bc60958839c7fa7
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-07-20 17:37:42 -0300

    aula 11.1

--- front-end/bloco-11/aula11.1/test/src/App.js
+++ front-end/bloco-11/aula11.1/test/src/App.js
@@ -1,34 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import Description from './Component';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-  
-  import React from 'react';
-
-class ReactClass extends React.Component {
-  render() {
-    return (
-      <h1>My first React Class Component!</h1>
-    )
-  }
+  return <Description />;
 }
 
 export default App;
