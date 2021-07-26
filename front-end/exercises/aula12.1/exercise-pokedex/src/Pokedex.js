eb7f6fe9225cdbc90413b986a7804838faac744b
Gustavo-Jezini <gustavojezini@gmail.com>
2021-07-26 09:36:02 -0300

    pokedex

--- front-end/exercises/aula12.1/exercise-pokedex/src/Pokedex.js
+++ front-end/exercises/aula12.1/exercise-pokedex/src/Pokedex.js
@@ -2,10 +2,32 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      index: 0,
+    };
+  };
+
+  handleClick() {
+    if (this.state.index >= (this.props.pokemons.length - 1)) {
+      this.setState({
+        index: 0,
+      });
+    }
+    else {
+      this.setState((state, _props) => ({
+        index: state.index + 1,
+      }));
+    }
+  };
     render() {
+      let indexPokemon = this.state.index;
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+              <button onClick={this.handleClick}>Próximo pokemon</button>
+                { <Pokemon pokemon={this.props.pokemons[indexPokemon]} /> }                
             </div>
         );
     }
