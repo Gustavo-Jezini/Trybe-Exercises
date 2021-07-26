eb769b9df47be13b306e8ad6e0bee166c8b9d377
Gustavo-Jezini <gustavojezini@gmail.com>
2021-07-26 13:24:05 -0300

    segunda parte da aula

--- front-end/bloco-12/aula12.2/ex-aula/src/Form.js
+++ front-end/bloco-12/aula12.2/ex-aula/src/Form.js
@@ -2,48 +2,55 @@ import React from 'react';
 import './App.css';
 
 class Form extends React.Component {
-  constructor() {
-    super()
+    constructor() {
+      super()
 
-    this.state = {
-      text: ''
+      this.state = {
+        input1: '',
+        input2: false,
+        options: 'Opcao 1',
+        text: ''
+      }
+
+      this.changeComponent = this.changeComponent.bind(this)
     }
 
-    this.changeComponent = this.changeComponent.bind(this)
-  }
+    changeComponent({ target }) {
+      const { name } = target;
+      const value = target.type === 'checkbox' ? target.checked : target.value;
+      this.setState({ [name]: value })
+    }
 
-  changeComponent(e) {
-    this.setState({ text: e.target.value })
-  }
-  render() {
+    render() {
 
 
-      return (
-        <form className='forms'>
-            <label className='labels'>
-                Aqui vem o 'select'
-                <select>
-                    <option>Opcao 1</option>
-                    <option>Opcao 2</option>
-                    <option>Opcao 3</option>
-                    <option>Opcao 4</option>
-                </select>
-            </label>
-            <label className='labels'>
-                Aqui ficará o primeiro input
-                <input type='text' onChange={this.changeComponent}></input>
-            </label>
-            <label className='labels'>
-                segundo input
-                <input type='button'></input>
-            </label>
-            <label className='labels'>
-                Finalmente o TextArea
-                <textarea></textarea>
-            </label>
-        </form>
-      )
-  }
+        return (
+            <fieldset className='forms'>
+              <legend>Formulario Basicao</legend>
+                <label className='labels'>
+                    Aqui vem o 'select'
+                    <select name='options' onChange={this.changeComponent}>
+                        <option>Opcao 1</option>
+                        <option>Opcao 2</option>
+                        <option>Opcao 3</option>
+                        <option>Opcao 4</option>
+                    </select>
+                </label>
+                <label className='labels'>
+                    Aqui ficará o primeiro input
+                    <input name='input1' type='text' onChange={this.changeComponent}></input>
+                </label>
+                <label className='labels'>
+                    segundo input
+                    <input name='input2' type='checkbox' onClick={this.changeComponent}></input>
+                </label>
+                <label className='labels'>
+                    Finalmente o TextArea
+                    <textarea name='text' onChange={this.changeComponent}></textarea>
+                </label>
+            </fieldset>
+        )
+    }
 }
 
 export default Form;
